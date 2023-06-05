52f98da71c24881af40bfdf920c9ae1459f3a4a9
KirillAdm <Adamovich-00@bk.ru>
2023-06-05 09:28:37 +0300

    feat:task 4

---
+++ task4.js
@@ -0,0 +1,15 @@
+let inn = 'inodm';
+console.log(inn[0],inn[2], inn[3]) 
+
+let str= 'inodm';
+let num = inn[1] ;
+console.log (num);
+
+let string = "Как видим, совпадений теперь два, а не три.";
+console.log (string[string.length-2]);
+
+let number = '12543';
+console.log(+number[0]+ +number[1] + +number[2] + +number[3] + +number[4])
+
+let st ='ii ini inni innni inoi inmi';
+console.log(st.match(/\w.{17}/));
