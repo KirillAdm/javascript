c01dc53caf9e93dcc9737f146678b67527003eb4
KirillAdm <Adamovich-00@bk.ru>
2023-06-05 14:43:33 +0300

    feat:dopdz 4

--- task4.js
+++ task4.js
@@ -13,3 +13,17 @@ console.log(+number[0]+ +number[1] + +number[2] + +number[3] + +number[4])
 
 let st ='ii ini inni innni inoi inmi';
 console.log(st.match(/\w.{17}/));
+
+let stringDop= 'aAXa aeffa aGha aza ax23a a3sSa';
+console.log(stringDop.match(/a[a-z][0-9]+a/g))
+
+let inno= "inno";
+let dom ="Dom";
+let Inno =(inno.replace(/i/, "I"));
+console.log (dom+Inno);
+
+let zad3 ='ini ioi idi inni iodi inoi';
+console.log(zad3.match(/i[a-z]{2}i/g));
+
+let zad4= "innoDom";
+console.log(zad4.split('').reverse('').join(''))
